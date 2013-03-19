aec8253000135e317d4356afde97a92a1d0d1c16
Niroj Shrestha <niroj.shrestha@cloudfactory.com>
2013-03-19 07:56:48 +0545

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Backbone::Application.config.secret_token = '6fa5deedd98410fefd04ff6ad2abaf6e65d18d9de66eb600f08a195707dc006310e7701be4f9151e5859648c9c1cacb18e6a291881301cb9389aa9d97a4708ec'
